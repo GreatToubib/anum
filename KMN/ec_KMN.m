ab46b064276be577bb28b721cb0b85912732dead
unknown <GreatToubib@MAGELLAN-55.users.magellan.fpms.ac.be>
2020-06-04 15:12:48 +0200

    last commit

--- KMN/ec_KMN.m
+++ KMN/ec_KMN.m
@@ -1,12 +1,15 @@
-function e = ec_KMN( yanal , ynum, space)
+function e = ec_KMN( yanal , ynum, space, plotgraph)
   
 global Jpattern solv nx ;
 diff=yanal-ynum ;
 for i =1:11
 a( i )=norm( diff(i,:));
-plot( space , diff( i , : ) , 'g' ) ;
-title( [' ec du simulateur KMN avec jpattern ' , num2str(Jpattern) , ' solv: ' , num2str(solv) , 'nbre de points: ' , num2str(nx) ])
-hold on
+if plotgraph==1
+    figure;
+    plot( space , diff( i , : ) , 'g' ) ;
+    %title( [' Simulateur KMN avec intégrateur ' , num2str(solv) , ' ,nbre de points: ' , num2str(nx), ' ,Jpattern: ' , num2str(Jpattern)  ])
+    hold on
+end
 end
 e=sum( a ) ;
 e=e/11;
